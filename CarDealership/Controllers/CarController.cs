fefac9fb68a46c2f0b8e4c7330ab23e0e3d9fbe0
Tyson Lackey <lackeyt90@gmail.com>
2020-07-21 10:57:17 -0700

    create html files and add controllers
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

---
+++ CarDealership/Controllers/CarController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Dealership.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Dealership.Controllers
+{
+    public class CarController : Controller
+    {
+
+      [HttpGet("/Cars")]
+      public ActionResult Index(double maxPrice = Double.PositiveInfinity)
+      {
+        List<Car> Cars = new List<Car>();
+        foreach(Car car in Car.GetAll())
+        {
+          if (car.Price < maxPrice)
+          {
+            Cars.Add(car);
+          }
+        }
+        return View(Cars);
+      }
+      
+      [HttpPost("/Cars")]
+      public ActionResult FilterPrice(string stringMaxPrice)
+      {
+        double maxPrice = double.Parse(stringMaxPrice);
+        return RedirectToAction("Index", maxPrice);
+      }
+    }
+}
--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Dealership.Models {
 
   public class Car
@@ -5,16 +7,28 @@ namespace Dealership.Models {
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+    private static List<Car> _instances = new List<Car> {};
 
     public Car(string makeModel, int price, int miles)
     {
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      _instances.Add(this);
     }
     public bool WorthBuying(int maxPrice)
     {
       return (Price < maxPrice);
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+
+    public void RemoveCar()
+    {
+      _instances.Remove(this);
     }
   }
 }
